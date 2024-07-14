26be858dcf686999ae9649952d53bc9af288f338
Cairo Rocha <cairor22@gmail.com>
2024-07-13 18:15:48 -0600

    final creo

--- ProyectoProduccion/Proyecto_Produccion/Models/Cpfijo.cs
+++ ProyectoProduccion/Proyecto_Produccion/Models/Cpfijo.cs
@@ -12,14 +12,17 @@ public partial class Cpfijo
     [Key]
     [Column("IDCPFijo")]
     public short Idcpfijo { get; set; }
-
+    
+    [Display(Name = "Demanda:")]
     public int Demanda { get; set; }
 
+    [Display(Name = "Inventario Pedido:")]
     public int InventarioPedido { get; set; }
-
+    [Display(Name = "Inventario Seguridad:")]
     public int InventarioSeguridad { get; set; }
 
     [StringLength(300)]
     [Unicode(false)]
+    [Display(Name = "Resultado:")]
     public string? Resultdado { get; set; }
 }
